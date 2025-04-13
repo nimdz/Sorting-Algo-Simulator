49b088bfc972b05379d45eecee3816a2f397b3ab
Nimsara De Zoysa <105286867+nimdz@users.noreply.github.com>
2025-04-13 14:55:49 +0530

    create index.js to centralize algorithm exports

---
+++ src/algorithms/index.js
@@ -0,0 +1,7 @@
+import { getBubbleSortAnimations } from "./bubbleSort";
+import { getSelectionSortAnimations } from "./selectionSort";
+
+export const algorithms = {
+  Bubble: getBubbleSortAnimations,
+  Selection: getSelectionSortAnimations,
+};
